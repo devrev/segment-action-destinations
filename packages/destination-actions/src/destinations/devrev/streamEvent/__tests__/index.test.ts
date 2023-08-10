5db8e9e2d26e3eb3646c46d573261437033dd8bf
Chris Brown <cb@devrev.ai>
2023-08-10 10:35:05 -0700

    Add code to decode the correct API endpoint from the JWT instead of hard coding it

--- packages/destination-actions/src/destinations/devrev/streamEvent/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/devrev/streamEvent/__tests__/index.test.ts
@@ -1,7 +1,7 @@
 import nock from 'nock'
 import { createTestEvent, createTestIntegration } from '@segment/actions-core'
 import Destination from '../../index'
-import { TrackEventsPublishBody, devrevApiPaths, devrevApiRoot } from '../../utils'
+import { TrackEventsPublishBody, devrevApiPaths, getBaseUrl } from '../../utils'
 import { settings, testAnonymousId, testContext, testEventPayload, testMessageId, testUserId } from '../../mocks'
 
 const testDestination = createTestIntegration(Destination)
@@ -9,7 +9,7 @@ const testDestination = createTestIntegration(Destination)
 describe('Devrev.streamEvent', () => {
   it('makes the correct API call to publish track events with default mapping', async () => {
     // Mock the publish API
-    nock(`${devrevApiRoot}`)
+    nock(`${getBaseUrl(settings)}`)
       .post(`${devrevApiPaths.trackEventsPublish}`)
       .reply(201, (_, body) => body)
 
